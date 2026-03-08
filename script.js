eacd800032ad9da5b109d9d05a107c9aab7e9979
Your Name <you@example.com>
2026-03-08 12:08:41 +0100

    Creación de mapa base AnyMap TS

--- script.js
+++ script.js
@@ -1,30 +1,14 @@
-// script.js
-
-import maplibregl from 'maplibre-gl';
-
-maplibregl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN'; // Replace with your Mapbox access token.
+import * as anywidget from 'anywidget';
+import * as maplibregl from 'maplibre-gl';
 
 const map = new maplibregl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v12',
-    center: [-4.7788, 36.7196],
-    zoom: 15
+  container: 'map',
+  style: 'https://demotiles.maplibre.org/style.json',
+  center: [0, 0],
+  zoom: 2
 });
 
-map.on('load', () => {
-    // Add a geojson source and layer here.
-    // Example:
-    // map.addSource('alcazaba', {
-    //     type: 'geojson',
-    //     data: './data/alcazaba.geojson'
-    // });
-    // map.addLayer({
-    //     id: 'alcazaba-layer',
-    //     type: 'fill',
-    //     source: 'alcazaba',
-    //     paint: {
-    //         'fill-color': '#007cbf',
-    //         'fill-opacity': 0.7
-    //     }
-    // });
+anywidget.register('AnyMap', (container) => {
+  container.appendChild(map.getCanvas());
+  return () => map.remove();
 });
